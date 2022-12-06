b1dee40c5b5fd80d1bce7f875eb01dbf6b625645
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-12-07 03:55:38 +0900

    WIP: disable stdin echo while running

--- src/Mimicry.jl
+++ src/Mimicry.jl
@@ -5,7 +5,8 @@ import Zygote
 import Flux
 import PolygonOps
 import StaticArrays
-import Plots
+import TERMIOS
+# import Plots
 
 include("Arena.jl")
 include("Agents.jl")
@@ -97,52 +98,79 @@ function main()
     polygon = StaticArrays.SVector.(xs,ys)
     inner = StaticArrays.SVector.([p[1] for p in inner_points],[p[2] for p in inner_points],)
     atexit(cleanup)
+    backup_termios = TERMIOS.termios()
+    TERMIOS.tcgetattr(stdin, backup_termios)
+    termios = TERMIOS.termios()
+    TERMIOS.tcgetattr(stdin, termios)
+     # Disable echo
+    termios.c_lflag &= ~TERMIOS.ECHO
+    TERMIOS.tcsetattr(stdin, TERMIOS.TCSANOW, termios)
     pop_size = 500
     bodies = [Agents.Body(0,0,0) for _ in 1:pop_size]
     for body in bodies
         Agents.randomBody!(body)
     end
     print("\033[?25l") # hide cursor
-    while true
-        alive = 0
-        # plt_arena = Arena.plot_arena()
-        # Plots.display(plt_arena)
-        for body in bodies
-            #Agents.plot_body!(body)
-            Agents.moveForward!(body)
-        end
-        positions = [(b.x, b.y) for b in bodies]
-        inside = [PolygonOps.inpolygon(p, polygon) for p in positions]
-        inside_inner = [PolygonOps.inpolygon(p, inner) for p in positions]
-        for (body, alive, crashed) in zip(bodies, inside, inside_inner)
-            if alive != 1 || crashed != 0
-                while PolygonOps.inpolygon((body.x,body.y), polygon) != 1 || PolygonOps.inpolygon((body.x, body.y), inner) != 0
-                    body.x = rand()*6 - 1
-                    body.y = rand()*4
-                    body.theta = rand()*2*pi
+    tpf = 0.001
+    Base.exit_on_sigint(false)
+    running = true
+    try
+        prev = time_ns()
+        while running
+            alive = 0
+            # plt_arena = Arena.plot_arena()
+            # Plots.display(plt_arena)
+            for body in bodies
+                #Agents.plot_body!(body)
+                Agents.moveForward!(body)
+            end
+            positions = [(b.x, b.y) for b in bodies]
+            inside = [PolygonOps.inpolygon(p, polygon) for p in positions]
+            inside_inner = [PolygonOps.inpolygon(p, inner) for p in positions]
+            for (body, alive, crashed) in zip(bodies, inside, inside_inner)
+                if alive != 1 || crashed != 0
+                    while PolygonOps.inpolygon((body.x,body.y), polygon) != 1 || PolygonOps.inpolygon((body.x, body.y), inner) != 0
+                        body.x = rand()*6 - 1
+                        body.y = rand()*4
+                        body.theta = rand()*2*pi
+                    end
                 end
             end
+            width = 121
+            height = 44
+            canvas = UnicodePlots.BrailleCanvas(height, width, origin_y=-1.2, origin_x=-1.2, height=4.4, width=6.4)
+            for (before, after) in zip(polygon[1:end-1], polygon[2:end])
+                UnicodePlots.lines!(canvas, before[1], before[2], after[1], after[2]; color=:cyan)
+            end
+            for (before, after) in zip(inner[1:end-1], inner[2:end])
+                UnicodePlots.lines!(canvas, before[1], before[2], after[1], after[2]; color=:cyan)
+            end
+            plt = UnicodePlots.Plot(canvas)
+            UnicodePlots.scatterplot!(plt, [b.x for b in bodies], [b.y for b in bodies], width=80, height=height)
+            println(UnicodePlots.show(plt))
+            current = time_ns()
+            alpha = 0.9
+            tpf = tpf * alpha + (1 - alpha) * (current - prev)
+            println(1/(tpf/1.0e9))
+            print(string("\033[s\033[",height+3,"A")) # move up height+3 lines
+            prev = current
+            # sleep(0.1)
+            #Plots.display(plt)
+            # x :: Vector{Float32} = [0.5, 0.3, 0.2]
+            # _, back = Zygote.pullback(forward, x, w)
+            # _ = back(1.0)
+            # println(y)
+            # println((dx,dw))
         end
-        width = 121
-        height = 44
-        canvas = UnicodePlots.BrailleCanvas(height, width, origin_y=-1.2, origin_x=-1.2, height=4.4, width=6.4)
-        for (before, after) in zip(polygon[1:end-1], polygon[2:end])
-            UnicodePlots.lines!(canvas, before[1], before[2], after[1], after[2]; color=:cyan)
+    catch e
+        if isa(e, Core.InterruptException)
+            running = false
+            TERMIOS.tcsetattr(stdin, TERMIOS.TCSANOW, backup_termios)
+            sleep(0.1)
+            println("\033[uexiting")
+        else
+            throw(e)
         end
-        for (before, after) in zip(inner[1:end-1], inner[2:end])
-            UnicodePlots.lines!(canvas, before[1], before[2], after[1], after[2]; color=:cyan)
-        end
-        plt = UnicodePlots.Plot(canvas)
-        UnicodePlots.scatterplot!(plt, [b.x for b in bodies], [b.y for b in bodies], width=80, height=height)
-        println(UnicodePlots.show(plt))
-        print(string("\033[",height+2,"A")) # move up hight+2 lines
-        sleep(0.1)
-        #Plots.display(plt)
-        # x :: Vector{Float32} = [0.5, 0.3, 0.2]
-        # _, back = Zygote.pullback(forward, x, w)
-        # _ = back(1.0)
-        # println(y)
-        # println((dx,dw))
     end
 end
 
